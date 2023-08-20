e6a0afa1a93fc89d6381b5ffbd97ececd5dff181
Lammyy5 <reliford2010@gmail.com>
2023-08-19 17:56:58 -0700

    Added code and comments so rock paper scissors functions

--- Main/script.js
+++ Main/script.js
@@ -1,6 +1,8 @@
 var wins = 0;
 var ties = 0;
 var losses = 0;
+// connects message id to be able to add text from javascript
+var messageEl = document.querySelector('#message');
 
 // Array of options for computer to pick from
 var options = ["R", "P", "S"];
@@ -11,6 +13,7 @@ function playGame(){
 // if no choice is entered the game ends 
   if(!userChoice){
     console.log('Game Ended');
+    messageEl.textContent = "The game was ended. Have a nice day"
     return;
   }
   // turns all user entries to uppercase 
@@ -20,6 +23,39 @@ function playGame(){
   var computerChoice = options[Math.floor(Math.random() * options.length)];
 // alerts users of the computers choice
   window.alert('The computer chose ' + computerChoice);
+// if the user choice is the same as the computer choice alert that its a tie
+  if(userChoice === computerChoice) {
+    ties++;
+    window.alert("It's a tie!")
+    // checks user choice and computer choice for the winner
+    // user win options
+  } else if (
+    //   || = or 
+  (userChoice === "R" && computerChoice === 'S') ||
+  (userChoice === "P" && computerChoice === 'R') ||
+  (userChoice === "S" && computerChoice === 'P') 
+  ) {
+    // Logs the wins of the user
+    wins++;
+    window.alert('You win!');
+    // Logs loss if conditions aren't meet
+  }else {
+    losses++;
+    window.alert('You lose');
+  }
+  // Alerts users of the score
+  window.alert('Stats: Wins: ' + wins + ' Losses: ' + losses + ' Ties: ' + ties);
+// Options for if the user wants to play again
+  var playAgain = window.confirm('Play Again?');
+// Calls function again in the case that the user wants to play again
+  if(playAgain){
+    playGame();
+  }
+  // returns if the user 
+  else {
+    messageEl.textContent = "The game was ended. Have a nice day"
+    return;
+  }
 
 }
 
